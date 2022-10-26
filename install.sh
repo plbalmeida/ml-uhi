970dc0d9f2032f0b93d11a795c00774c0a05aeae
plbalmeida <plbalmeida@usp.br>
2022-10-26 20:47:40 +0000

    updating setup

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ virtualenv -p python3 venv;
 . venv/bin/activate;
 
 # installing pre-commit to update requirements.txt file automatically
-pip3 install -r requirements.txt; 
 pip3 install pre-commit;
 pre-commit install;
 
