e1c1b4bf8094d48eb9799548e4120c8ff832523e
plbalmeida <plbalmeida@usp.br>
2022-10-29 18:32:25 -0300

    ingress CIDR block changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "ssh-allowed" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["189.110.89.5/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     
     tags = {
